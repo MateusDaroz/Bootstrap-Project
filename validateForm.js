74ca31f82d0fb5c351b1f8129fd14877d49e8f59
MateusDaroz <matlicoop@gmail.com>
2024-10-29 01:00:15 -0300

    Add: toast, fixed responsivity and add some items on index

--- validateForm.js
+++ validateForm.js
@@ -1,19 +1,29 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
+  'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+  const toastElement = document.getElementById('liveToast');
+  const toast = new bootstrap.Toast(toastElement);
+
+  // Loop over forms and apply validation
+  Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+          // Prevent default form submission
+          event.preventDefault();
+          event.stopPropagation();
+
+          // If form is valid, show toast and submit form after 2 seconds
+          if (form.checkValidity()) {
+              toast.show();
+
+              // Wait for 2 seconds and submit form
+              setTimeout(() => {
+                  form.submit();
+              }, 3000);
+          }
+
+          form.classList.add('was-validated');
+      }, false);
+  });
+})();
